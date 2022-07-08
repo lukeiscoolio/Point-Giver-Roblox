f650708b47a2776ee89c60f9d0eb5013c69e981a
luke <98642495+lukeiscoolio@users.noreply.github.com>
2022-07-08 16:38:20 +0100

    Update and rename README.md to README.lua

---
+++ Ungroup me/README.lua
@@ -0,0 +1,14 @@
+--[[
+Hello! Please follow these steps:
+
+1. Do not delete the part for workspace because that is how you get the points when you step on it.
+
+2. Ungroup everything and put it in the right place.
+
+3. Please make sure that you rename the ServerScriptService and the part script to what you want it to be called, if you want it to still be called "Wins" please do not touch any of the scripts!
+
+Please keep in mind that I do not think this saves!
+
+Thank you for using my model!!
+
+]]
